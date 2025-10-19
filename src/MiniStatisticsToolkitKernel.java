b351c6f1daf826ed4a19867391fd30d97fc7ff6d
RuoxiZ03 <aiyoyo1111@icloud.com>
2025-10-19 15:46:48 -0400

    Add implementation for MiniStatisticsToolkit, Kernel, and Test files

--- src/MiniStatisticsToolkitKernel.java
+++ src/MiniStatisticsToolkitKernel.java
@@ -1,3 +1,34 @@
-public class MiniStatisticsToolkitKernel {
-    
+/**
+ * Kernel interface for a simple statistics toolkit.
+ * @author Ruoxi Zhang
+ */
+
+public interface MiniStatisticsToolkitKernel {
+
+    /**
+     * Adds a data point to the toolkit.
+     * 
+     * @param value
+     *            The numeric value to add
+     * @updates this
+     * @ensures this includes value
+     */
+    void addData(double value);
+
+    /**
+     * Removes the most recently added data point.
+     * 
+     * @updates this
+     * @requires this is not empty
+     * @ensures last value is removed
+     */
+    void removeLast();
+
+    /**
+     * Returns the number of data points.
+     * 
+     * @return count
+     * @ensures length of data = count
+     */
+    int length();
 }
